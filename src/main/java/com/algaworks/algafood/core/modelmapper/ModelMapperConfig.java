2d0eb80840a4768c08b065360d30a3181b7c70da
Fernando Alexandre Wahl <fernando.wahl@tbdc.com.br>
2025-01-04 14:46:08 -0300

    inclusão de endereçao na apresentação do restaurante

--- src/main/java/com/algaworks/algafood/core/modelmapper/ModelMapperConfig.java
+++ src/main/java/com/algaworks/algafood/core/modelmapper/ModelMapperConfig.java
@@ -4,7 +4,9 @@ import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import com.algaworks.algafood.api.model.EnderecoModel;
 import com.algaworks.algafood.api.model.RestauranteModel;
+import com.algaworks.algafood.domain.model.Endereco;
 import com.algaworks.algafood.domain.model.Restaurante;
 
 @Configuration
@@ -16,6 +18,11 @@ public class ModelMapperConfig {
 
     modelMapper.createTypeMap(Restaurante.class, RestauranteModel.class)
         .addMapping(Restaurante::getTaxaFrete, RestauranteModel::setPrecoFrete);
+
+    var enderecoToEnderecoModelTypeMap = modelMapper.createTypeMap(Endereco.class, EnderecoModel.class);
+    enderecoToEnderecoModelTypeMap.<String>addMapping(
+        enderecoSrc -> enderecoSrc.getCidade().getEstado().getNome(),
+        (enderecoDest, value) -> enderecoDest.getCidade().setNomeEstado(value));
 
     return modelMapper;
   }
