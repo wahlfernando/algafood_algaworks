648a5a865cbaede614896635ceae60530e16c021
Fernando Alexandre Wahl <wahlfernando@yahoo.com.br>
2026-04-28 21:17:10 -0300

    Add Cross-Origin support to RestauranteController and implement jQuery-based frontend for restaurant listing

--- src/main/java/com/algaworks/algafood/api/controller/RestauranteController.java
+++ src/main/java/com/algaworks/algafood/api/controller/RestauranteController.java
@@ -2,21 +2,15 @@ package com.algaworks.algafood.api.controller;
 
 import java.util.List;
 
+import com.fasterxml.jackson.annotation.JsonView;
 import jakarta.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.json.MappingJacksonValue;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.algaworks.algafood.api.assembler.RestauranteModelAssembler;
 import com.algaworks.algafood.api.assembler.RestauranteModelDisAssembler;
@@ -31,6 +25,7 @@ import com.algaworks.algafood.domain.model.Restaurante;
 import com.algaworks.algafood.domain.repository.RestauranteRepository;
 import com.algaworks.algafood.domain.service.CadastroRestauranteService;
 
+@CrossOrigin
 @RestController
 @RequestMapping(value = "/restaurantes")
 public class RestauranteController {
@@ -47,33 +42,19 @@ public class RestauranteController {
     @Autowired
     private RestauranteModelDisAssembler restauranteModelDisAssembler;
 
+
     @GetMapping()
-    public MappingJacksonValue listar(@RequestParam(required = false) String projecao) {
-        List<Restaurante> restaurantes = restauranteRepository.findAll();
-        List<RestauranteModel> restauranteModel = restauranteModelAssembler.toCollectionModel(restaurantes);
-
-        MappingJacksonValue restauranteWrapper = new MappingJacksonValue(restauranteModel);
-
-        if ("apenas-nome".equals(projecao)) {
-            restauranteWrapper.setSerializationView(RestauranteView.ApenasNome.class);
-        } else if ("resumo".equals(projecao)) {
-            restauranteWrapper.setSerializationView(RestauranteView.Resumo.class);
-        }
-
-        return restauranteWrapper;
-
+    public List<RestauranteModel> listar() {
+        return
+                restauranteModelAssembler.toCollectionModel(restauranteRepository.findAll());
     }
 
-    // @GetMapping()
-    // public List<RestauranteModel> listar() {
-    // return
-    // restauranteModelAssembler.toCollectionModel(restauranteRepository.findAll());
-    // }
-    // @JsonView(RestauranteView.Resumo.class)
-    // @GetMapping(params = "projecao=resumo")
-    // public List<RestauranteModel> listarResumido() {
-    // return listar();
-    // }
+    @JsonView(RestauranteView.Resumo.class)
+    @GetMapping(params = "projecao=resumo")
+    public List<RestauranteModel> listarResumido() {
+        return listar();
+    }
+
     @GetMapping("/{restauranteId}")
     public RestauranteModel buscar(@PathVariable Long restauranteId) {
         Restaurante restaurante = cadastroRestaurante.buscarOuFalhar(restauranteId);
@@ -91,12 +72,12 @@ public class RestauranteController {
         }
     }
 
+
     @PutMapping("/{restauranteId}")
     public RestauranteModel atualizar(@PathVariable Long restauranteId,
-            @RequestBody @Valid RestauranteInput restauranteInput) {
+                                      @RequestBody @Valid RestauranteInput restauranteInput) {
 
         Restaurante restauranteAtual = cadastroRestaurante.buscarOuFalhar(restauranteId);
-
         restauranteModelDisAssembler.copyToDomainObject(restauranteInput,
                 restauranteAtual);
 
